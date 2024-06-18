bc364c7c52098361e70cfac03bfff019cf045e7d
Sathish Thangadurai <sathish.thangadurai@photon.com>
2024-06-18 17:55:47 +0530

    added swagger

--- src/main/java/com/photon/SwaggerConfiguration.java
+++ src/main/java/com/photon/SwaggerConfiguration.java
@@ -3,8 +3,14 @@ package com.photon;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.properties.SwaggerUiConfigParameters;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Objects;
 
 @Configuration
 @OpenAPIDefinition
@@ -17,5 +23,18 @@ public class SwaggerConfiguration  {
                         .title("Api Gateway Microservice")
                         .version("0.1")
         );
+    }
+
+    @Bean
+    public CommandLineRunner openApiGroups(
+            RouteDefinitionLocator locator,
+            SwaggerUiConfigParameters swaggerUiParameters) {
+        return args -> Objects.requireNonNull(locator
+                        .getRouteDefinitions().collectList().block())
+                .stream()
+                .map(RouteDefinition::getId)
+                .filter(id -> id.matches(".*-service"))
+                .map(id -> id.replace("-service", ""))
+                .forEach(swaggerUiParameters::addGroup);
     }
 }
