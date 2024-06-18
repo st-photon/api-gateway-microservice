bfa18e44ee3043ab32d825d504e2c33e7b6e4a42
Sathish Thangadurai <sathish.thangadurai@photon.com>
2024-06-18 17:15:21 +0530

    api gateway swagger

--- src/main/java/com/photon/SwaggerConfiguration.java
+++ src/main/java/com/photon/SwaggerConfiguration.java
@@ -1,6 +1,21 @@
 package com.photon;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition
 public class SwaggerConfiguration  {
 
-
+    @Bean
+    public OpenAPI baseOpenAPI(){
+        return new OpenAPI().info(
+                new Info()
+                        .title("Api Gateway Microservice")
+                        .version("0.1")
+        );
+    }
 }
